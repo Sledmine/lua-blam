8316ab10e1d0813f66e4de4497e8c72be37fc241
Sledmine <angelherxd@hotmail.com>
2019-06-29 14:26:28 -0600

    updated library, new documentation

--- README.md
+++ README.md
@@ -4,15 +4,56 @@
 Is a Lua library for scripting that allows you to handle data of the game objects in memory of Halo Custom Edition.
 
 ### What i can do with it?
- - Manipulate objects data as scripting table/object properties.
+Manipulate objects data as scripting table/object properties.
+
+Supported game objects in this version:
+- Biped:
+    -
 
 ### Install LuaBlam
-You can download "LuaBlam.lua" library of this repository and move it in to "Halo Custom Edition\Lua", is really recommended to install it using "Mercury" check more info about in this GitHub repository.
+You can download "luablam.lua" library of this repository and move it in to "Halo Custom Edition\Lua".
+Is really recommended to install it using "Mercury - Package Manager" check more info about it in this GitHub repository.
 
 ### Implementing LuaBlam
 Add a require line into your Lua script:
 
 ```lua
-local blam = require "luablam"
+
+-- Blam text example for Chimera - 572/582 Lua scripting.
+
+local blam = require("luablam") -- Imported from Lua folder.
+
+clua_version = 2.042 -- Set API version.
+
+set_callback("command", "onCommand") -- Register callback, run function "onCommand" when triggered.
+
+function onCommand(command)
+    if (command == "invi") then -- If we wan to get or unget invisible then...
+
+        local playerAddress = get_dynamic_player() -- Get current player memory address.
+
+        if (playerAddress ~= nil) then -- There are no errors getting player address then...
+
+            -- Get biped data of the specified object address (giving current player address).
+            local playerBiped = blam.biped(playerAddress)
+
+            if (playerBiped ~= nil) then -- If there are no errors getting player biped data then...
+                
+                -- Save player invisible property into "playerIsInvisible" variable
+                playerIsInvisible = playerBiped.invisible
+
+                -- Refresh biped data info giving table/object with invisible property with "not" operator to invert value
+                blam.biped(playerAddress, { invisible = not(playerIsInvisible) })
+
+            end
+        end
+
+        return false
+    end
+end
 ```
+
+![Before "invi" command](https://imgur.com/W8Vyw0F)
+
+![After "invi" command](https://imgur.com/W8Vyw0F)
 
---
+++ luablam.lua
@@ -0,0 +1,83 @@
+-- Blam library for Chimera Lua scripting.
+-- Version 1.0
+-- This library is intended to help and improve object memory handle using Lua, to make a standar for modding and avoid large script files.
+
+luablam = {}
+
+local function ifbit(bit)
+    if (bit == 0) then
+        return false
+    else
+        return true
+    end
+end
+
+local function biped(address, data)
+    local objectMemory = address
+    local bipd
+    if (objectMemory ~= nil) then
+        bipd = {
+            tag_id = read_dword(objectMemory),
+            health = read_float(objectMemory + 0xE0),
+            shield = read_float(objectMemory + 0xE4),
+            x = read_float(objectMemory + 0x5C),
+            y = read_float(objectMemory + 0x60),
+            z = read_float(objectMemory + 0x64),
+            xVel = read_float(objectMemory + 0x68),
+            yVel = read_float(objectMemory + 0x6C),
+            zVel = read_float(objectMemory + 0x70),
+            pitch = read_float(objectMemory + 0x74),
+            yaw = read_float(objectMemory + 0x78),
+            animation = read_word(objectMemory + 0xD0),
+            animationTimer = read_word(objectMemory + 0xD2),
+            crouch = read_byte(objectMemory + 0x2A0),
+            weaponSlot = read_byte(objectMemory + 0x2A1),
+            flashlight = ifbit(read_bit(objectMemory + 0x204, 19)),
+            invisible = ifbit(read_bit(objectMemory + 0x204, 4)),
+            currentNade = read_byte(objectMemory + 0x31C),
+            fragGrenades = read_byte(objectMemory + 0x31E),
+            plasmaGrenades = read_byte(objectMemory + 0x31F),
+            zoomLevel = read_byte(objectMemory + 0x320),
+            currentWeapon = read_dword(objectMemory + 0x118),
+            region_permutation_1 = read_char(objectMemory + 0x180),
+            region_permutation_2 = read_char(objectMemory + 0x181),
+        }
+    end
+    local bipedWritableValues = {
+        tag_id = function() write_dword(objectMemory, data.tag_id) end,
+        x = function() write_float(objectMemory + 0x5C, data.x) end,
+        y = function() write_float(objectMemory + 0x60, data.y) end,
+        z = function() write_float(objectMemory + 0x64, data.z) end,
+        xVel = function() write_float(objectMemory + 0x68, data.xVel) end,
+        yVel = function() write_float(objectMemory + 0x6C, data.yVel) end,
+        zVel = function() write_float(objectMemory + 0x70, data.zVel) end,
+        pitch = function() write_float(objectMemory + 0x74, data.pitch) end,
+        yaw = function() write_float(objectMemory + 0x78, data.yaw) end,
+        animation = function() write_word(objectMemory + 0xD0, data.animation) end,
+        animationTimer = function() write_word(objectMemory + 0xD2, data.animationTimer) end,
+        crouch = function() write_byte(objectMemory + 0x2A0, data.crouch) end,
+        weaponSlot = function() write_byte(objectMemory + 0x2A1, data.weaponSlot) end,
+        flashlight = function() write_bit(objectMemory + 0x204, 19, data.flashlight) end,
+        invisible = function() write_bit(objectMemory + 0x204, 4, data.invisible) end,
+        currentNade = function() write_byte(objectMemory + 0x31C, data.currentNade) end,
+        fragGrenades = function() write_byte(objectMemory + 0x31E, data.fragGrenades) end,
+        plasmaGrenades = function() write_byte(objectMemory + 0x31F, data.plasmaGrenades) end,
+        zoomLevel = function() write_byte(objectMemory + 0x320, data.zoomLevel) end,
+        region_permutation_1 = function() write_char(objectMemory + 0x180, data.region_permutation_1) end,
+        region_permutation_2 = function() write_char(objectMemory + 0x181, data.region_permutation_2) end
+    }
+    if (data ~= nil) then
+        for i,v in pairs(data) do
+            for j,k in pairs(bipedWritableValues) do
+                if (i == j) then
+                    bipedWritableValues[i]()
+                end
+            end
+        end
+    end
+    return bipd
+end
+
+luablam.biped = biped
+return luablam
+
