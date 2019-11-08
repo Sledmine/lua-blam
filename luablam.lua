86f1e3f5b65a5deacf57995b87c4ba0ccb3b36aa
Sledmine <angelherxd@hotmail.com>
2019-11-07 23:52:00 -0700

    tons of new stuff, updated readme

--- luablam.lua
+++ luablam.lua
@@ -1,62 +1,77 @@
--- Blam library for Chimera Lua scripting.
--- Version 2.0
+------------------------------------------------------------------------------
+-- Blam library for Chimera/SAPP Lua scripting.
+-- Authors: Gelatinoso, Sledmine
+-- Version: 1.0
 -- This library is intended to help and improve object memory handle using Lua, to make a standar for modding and avoid large script files.
+------------------------------------------------------------------------------
 
 --[[
-[Changelog]
 
-2.0:
-    Insane optimization, expanded object structures and available properties, better implentation for reading and writing data
+        THIS IS AN UNNOFICIAL VERSION, JUST FOR TESTING, DO NOT DISTRIBUTE THIS LIBRARY
+
+Changelog:
+
+2.1: New tag data handle added, implemented string metable.
+
+    - Added support for ui widgets definition (UNCOMPLETE, MORE VALUES REQUIRED).
+    - Added support for weapon hud interfaces (UNSTABLE, HARDCODED TO FIRST CROSSHAIR ELEMENT).
+    - Added support for unicode string lists.
+    - Added playerIsLookingAt function.
+    - Object dataReclaimer now supports unicode string list (10 = UStringL).
+    - Object dataReclaimer now supports scenery palette list (11 = SceneryPL). -- Only for reading
+    - Object dataReclaimer now supports child ui widgets list (12 = ChildWL). -- Only for reading
+
+2.0: Insane optimization, expanded object structures and available properties, better implentation for reading and writing data.
+
     - Introducing "dataReclaimer", this is an array with specific values for structure fields
     - Improved bit2bool convertion now only 1 as bit value is being interpreted as true any other value will be always false.
 
-    To reduce large value names in every object and avoid being repetitive, a "dataReclaimer" is needed, made of multiple values that are arrays (instead of using properties such as "offset", "type", etc)
-
-    A dataReclaimer is an specific format of data that aims to "reclaim" data for writing or reading
-
-    Here is an example of a dataReclaimer for an object "x"...
-
-    local exampleStructure = {
-        dataReclaimer = {0x213, 0}
-    }
-
-    Where dataReclaimer is an array with a specific ordered data:
+    dataReclaimer is an array with a specific ordered data:
 
     dataReclaimer[0] -- Is the offset in the memory to read/write a value
     dataReclaimer[1] -- Specify the type of value to read/write
 
-    0 = bit
-    1 = byte
-    2 = short
-    3 = word
-    4 = int
-    5 = dword
-    6 = float
-    7 = double
-    8 = char
-    9 = string
+    0 = Bit
+    1 = Byte
+    2 = Short
+    3 = Word
+    4 = Int
+    5 = Dword
+    6 = Float
+    7 = Double
+    8 = Char
+    9 = String
 
     If we want to use a specific bit position then an extra value is sent in the array:
 
     dataReclaimer = {0x213, 0, 8}
 
     This means the position of the bit that is being intented to read/write.
+    
+     (0 = Biped) (1 = Vehicle) (2 = Weapon) (3 = Equipment) (4 = Garbage) (5 = Projectile) (6 = Scenery) (7 = Machine) (8 = Control) (9 = Light Fixture) (10 = Placeholder) (11 = Sound Scenery)
 
-1.1:
-    Changes for writable and readable biped properties.
+1.1: Changes for writable and readable biped properties.
 
-1.0:
-    First realease for Flood 09, biped handle ready.
+1.0: First realease for Flood 09, biped handle ready.
+
 ]]
 
 luablam = {}
 
-local function bit2bool(bit)-- Convert bits into boolean values (Writing true or false is equal to 1 or 0 but not when reading)
+getmetatable("").__index = function(str,i) -- Allow the script to handle strings as an array
+    if type(i) == 'number' then
+        return string.sub(str,i,i)
+    else
+        return string[i]
+    end
+end
+
+local function bit2bool(bit) -- Convert bits into boolean values (Writing true or false is equal to 1 or 0 but not when reading)
     if (bit == 1) then return true end
     return false
 end
 
-local function dispatchOperation(dataReclaimer, operation, value)-- Decide wich operation will be performed by the the "reclaimer" object
+local function dispatchOperation(dataReclaimer, operation, value) -- Decide wich operation will be performed by the the "reclaimer" object
     if (operation == true) then -- Looking for writing
         if (dataReclaimer[2] == 0) then -- Bit
             write_bit(dataReclaimer[1], dataReclaimer[#dataReclaimer], value)
@@ -78,6 +93,26 @@ local function dispatchOperation(dataReclaimer, operation, value)-- Decide wich 
             write_char(dataReclaimer[1], value)
         elseif (dataReclaimer[2] == 9) then -- String
             write_string(dataReclaimer[1], value)
+        elseif (dataReclaimer[2] == 10) then -- UStringL
+            local stringCount = read_byte(dataReclaimer[1] - 0x4)
+            local stringListAddress = read_dword(dataReclaimer[1])
+            local incomingStrings = #value
+            local availableStrings = incomingStrings
+            if (incomingStrings > stringCount) then
+                availableStrings = stringCount
+            end
+            for i = 1,availableStrings do
+                --local stringSize = read_dword(stringListAddress) / 2
+                local stringValueAddress = read_dword(stringListAddress + 0xC)
+                for j = 1,#value[i] do
+                    write_string(stringValueAddress, value[i][j])
+                    stringValueAddress = stringValueAddress + 0x2
+                    if (j == #value[i]) then
+                        write_byte(stringValueAddress, 0x0)
+                    end
+                end
+                stringListAddress = stringListAddress + 0x14
+            end
         end
     else -- Looking for reading
         if (dataReclaimer[2] == 0) then -- Is bit type
@@ -100,73 +135,151 @@ local function dispatchOperation(dataReclaimer, operation, value)-- Decide wich 
             return read_char(dataReclaimer[1])
         elseif (dataReclaimer[2] == 9) then -- String
             return read_string(dataReclaimer[1])
+        elseif (dataReclaimer[2] == 10) then -- UStringL
+            local stringCount = read_byte(dataReclaimer[1] - 0x4)
+            local stringListAddress = read_dword(dataReclaimer[1])
+            local stringList = {}
+            for i = 1,stringCount do
+                local stringSize = read_dword(stringListAddress) / 2
+                local stringValueAddress = read_dword(stringListAddress + 0xC)
+                local stringValue = ""
+                for j = 1,stringSize do
+                    local charValue = read_string(stringValueAddress)
+                    if (charValue == "") then break end
+                    stringValue = stringValue .. charValue
+                    stringValueAddress = stringValueAddress + 0x2
+                end
+                stringList[i] = stringValue
+                stringListAddress = stringListAddress + 0x14
+            end
+            return stringList
+        elseif (dataReclaimer[2] == 11) then -- SceneryPL
+            local sceneryCount = read_dword(dataReclaimer[1] - 0x4)
+            local sceneryListAddress = read_dword(dataReclaimer[1])
+            local sceneryList = {}
+            for i = 1,sceneryCount do
+                local sceneryIndex = read_dword(sceneryListAddress + 0xC)
+                sceneryList[i] = sceneryIndex
+                sceneryListAddress = sceneryListAddress + 0x30
+            end
+            return sceneryList
+        elseif (dataReclaimer[2] == 12) then -- ChildWL
+            local childWidgetCount = read_dword(dataReclaimer[1] - 0x4)
+            local childWidgetListAddress = read_dword(dataReclaimer[1])
+            local childWidgetList = {}
+            for i = 1,childWidgetCount do
+                local childWidgetIndex = read_dword(childWidgetListAddress + 0xC)
+                childWidgetList[i] = childWidgetIndex
+                childWidgetListAddress = childWidgetListAddress + 0x50
+            end
+            return childWidgetList
         end
     end
 end
 
 local objectStructure = {
-    tagId = {0, 5}, -- WORKING
-    collision = {0x10, 0, 0}, -- NOT TESTED
-    isOnGround = {0x10, 0, 1}, -- WORKING
-    ignoreGravity = {0x10, 0, 2}, -- NOT TESTED
-    isOutSideMap = {0x10, 0, 21}, -- NOT TESTED
-    collideable = {0x10, 0, 24}, -- NOT TESTED
-    health = {0xE0, 6}, -- WORKING
-    shield = {0xE4, 6}, -- WORKING
-    x = {0x5C, 6}, -- NOT TESTED
-    y = {0x60, 6}, -- NOT TESTED
-    z = {0x64, 6}, -- NOT TESTED
-    xVel = {0x68, 6}, -- WORKING
-    yVel = {0x6C, 6}, -- WORKING
-    zVel = {0x70, 6}, -- WORKING
-    pitch = {0x74, 6}, -- NOT TESTED
-    yaw = {0x78, 6}, -- NOT TESTED
-    roll = {0x7C, 6}, -- NOT TESTED
-    xScale = {0x80, 6}, -- NOT TESTED
-    yScale = {0x84, 6}, -- NOT TESTED
-    zScale = {0x88, 6}, -- NOT TESTED
-    pitchVel = {0x8C, 6}, -- NOT TESTED
-    yawVel = {0x90, 6}, -- NOT TESTED
-    rollVel = {0x94, 6}, -- NOT TESTED
-    type = {0xB4, 3}, -- WORKING (0 = Biped) (1 = Vehicle) (2 = Weapon) (3 = Equipment) (4 = Garbage) (5 = Projectile) (6 = Scenery) (7 = Machine) (8 = Control) (9 = Light Fixture) (10 = Placeholder) (11 = Sound Scenery)
-    animation = {0xD0, 3}, -- WORKING
-    animationTimer = {0xD2, 3}, -- WORKING
-    regionPermutation1 = {0x180, 1}, -- WORKING
-    regionPermutation2 = {0x181, 1}, -- WORKING
-    regionPermutation3 = {0x182, 1}, -- WORKING
-    regionPermutation4 = {0x183, 1}, -- WORKING
-    regionPermutation5 = {0x184, 1}, -- WORKING
-    regionPermutation6 = {0x185, 1}, -- WORKING
-    regionPermutation7 = {0x186, 1}, -- WORKING
-    regionPermutation8 = {0x187, 1}-- WORKING
+    tagId = {0, 5}, 
+    collision = {0x10, 0, 0}, 
+    isOnGround = {0x10, 0, 1}, 
+    ignoreGravity = {0x10, 0, 2}, 
+    isOutSideMap = {0x12, 0, 5}, 
+    collideable = {0x10, 0, 4}, 
+    health = {0xE0, 6}, 
+    shield = {0xE4, 6}, 
+    x = {0x5C, 6}, 
+    y = {0x60, 6}, 
+    z = {0x64, 6}, 
+    xVel = {0x68, 6}, 
+    yVel = {0x6C, 6}, 
+    zVel = {0x70, 6}, 
+    pitch = {0x74, 6}, 
+    yaw = {0x78, 6}, 
+    roll = {0x7C, 6}, 
+    xScale = {0x80, 6}, 
+    yScale = {0x84, 6}, 
+    zScale = {0x88, 6}, 
+    pitchVel = {0x8C, 6}, 
+    yawVel = {0x90, 6}, 
+    rollVel = {0x94, 6}, 
+    type = {0xB4, 3},
+    animation = {0xD0, 3}, 
+    animationTimer = {0xD2, 3}, 
+    regionPermutation1 = {0x180, 1}, 
+    regionPermutation2 = {0x181, 1}, 
+    regionPermutation3 = {0x182, 1}, 
+    regionPermutation4 = {0x183, 1}, 
+    regionPermutation5 = {0x184, 1}, 
+    regionPermutation6 = {0x185, 1}, 
+    regionPermutation7 = {0x186, 1}, 
+    regionPermutation8 = {0x187, 1} 
 }
 
 local bipedStructure = {
-    invisible = {0x204, 0, 4}, -- WORKING
-    noDropItems = {0x204, 0, 20}, -- WORKING
-    flashlight = {0x204, 0, 19}, -- WORKING
-    crouchHold = {0x208, 0, 0}, -- WORKING
-    jumpHold = {0x208, 0, 1}, -- WORKING
-    flashlightKey = {0x208, 0, 4}, --WORKING
-    actionKey = {0x208, 0, 6}, -- WORKING
-    meleeKey = {0x208, 0, 7}, -- WORKING
-    reloadKey = {0x208, 0, 10}, -- WORKING
-    weaponPTH = {0x208, 0, 11}, -- WORKING
-    weaponSTH = {0x208, 0, 12}, -- WORKING
-    grenadeHold = {0x208, 0, 13}, -- WORKING, similar to weaponSTH.
-    actionKeyHold = {0x208, 0, 14}, -- WORKING
-    crouch = {0x2A0, 1}, -- WORKING
-    shooting = {0x284, 6}, -- WORKING, this is being read as a float, but acts like a boolean
-    weaponSlot = {0x2A1, 1}, -- WORKING, starts from 0.
-    zoomLevel = {0x320, 1}, -- WORKING, 255 if there is no actual zoom, zoom levels starts from 0.
-    invisibleScale = {0x37C, 6}, -- WORKING
-    primaryNades = {0x31E, 1}, -- WORKING
-    secondaryNades = {0x31F, 1}-- WORKING
+    invisible = {0x204, 0, 4},
+    noDropItems = {0x204, 0, 20}, 
+    flashlight = {0x204, 0, 19}, 
+    cameraX = {0x230, 6},
+    cameraY = {0x234, 6},
+    cameraZ = {0x238, 6},
+    crouchHold = {0x208, 0, 0}, 
+    jumpHold = {0x208, 0, 1},
+    actionKeyHold = {0x208, 0, 14},
+    actionKey = {0x208, 0, 6},
+    meleeKey = {0x208, 0, 7}, 
+    reloadKey = {0x208, 0, 10}, 
+    weaponPTH = {0x208, 0, 11},
+    weaponSTH = {0x208, 0, 12},
+    flashlightKey = {0x208, 0, 4},
+    grenadeHold = {0x208, 0, 13},
+    crouch = {0x2A0, 1}, 
+    shooting = {0x284, 6},
+    weaponSlot = {0x2A1, 1},
+    zoomLevel = {0x320, 1},
+    invisibleScale = {0x37C, 6}, 
+    primaryNades = {0x31E, 1}, 
+    secondaryNades = {0x31F, 1} 
+}
+
+local unicodeStringListStructure = {
+    count = {0x0, 1},
+    stringList = {0x4, 10}
+}
+
+local uiWidgetDefinitionStructure = {
+    type = {0x0,3},
+    controllerIndex = {0x2,3},
+    name = {0x4, 9},
+    boundsY = {0x24, 3},
+    boundsX = {0x26, 3},
+    height = {0x28, 3},
+    width = {0x2A, 3},
+    eventType = {0x03F0, 1},
+    childWidgetsCount = {0x03E0, 5},
+    childWidgetsList = {0x03E4, 12}
+}
+
+local weaponHudInterfaceStructure = {
+    crosshairs = {0x84, 3},
+    defaultBlue = {0x208, 1},
+    defaultGreen = {0x209, 1},
+    defaultRed = {0x20A, 1},
+    defaultAlpha = {0x20B, 1},
+    --flashingColor = {0x20C, 1},
+    sequenceIndex = {0x22A, 2}
+}
+
+local scenarioStructure = {
+    sceneryPaletteCount = {0x021C, 5},
+    sceneryPaletteList = {0x220, 11}
 }
 
 local availableObjectTypes = {
     object = {objectStructure},
-    biped = {objectStructure, bipedStructure}
+    biped = {objectStructure, bipedStructure},
+    uiWidgetDefinition = {uiWidgetDefinitionStructure},
+    weaponHudInterface = {weaponHudInterfaceStructure},
+    unicodeStringList = {unicodeStringListStructure},
+    scenario = {scenarioStructure}
 }
 
 local function proccessRequestedObject(desiredObject, address, properties)
@@ -212,6 +325,65 @@ local function biped(address, properties)
     return proccessRequestedObject("biped", address, properties)
 end
 
+local function uiWidgetDefinition(address, properties)
+    local tagDataAddress = read_dword(address + 0x14)
+    return proccessRequestedObject("uiWidgetDefinition", tagDataAddress, properties)
+end
+
+local function weaponHudInterface(address, properties)
+    local tagDataAddress = read_dword(address + 0x14)
+    return proccessRequestedObject("weaponHudInterface", tagDataAddress, properties)
+end
+
+local function unicodeStringList(address, properties)
+    local tagDataAddress = read_dword(address + 0x14)
+    return proccessRequestedObject("unicodeStringList", tagDataAddress, properties)
+end
+
+local function scenario(address, properties)
+    local tagDataAddress = read_dword(address + 0x14)
+    return proccessRequestedObject("scenario", tagDataAddress, properties)
+end
+
+local function playerIsLookingAt(target)
+	local sensitivity = 0.045 -- Max
+	local baseline_sensitivity = 0.012 -- Minimum for distance scaling.
+	local z_positive_offset = 0 -- Move sensitivity area up to the chest.
+	local function read_vector3d(Address) return read_float(Address), read_float(Address+0x4), read_float(Address+0x8) end
+	local m_object = get_dynamic_player()
+	local m_target_object = get_object(target)
+	if m_target_object and m_object then -- Both objects must exist.
+		local player_x, player_y, player_z = read_vector3d(m_object + 0xA0)
+		local camera_x, camera_y, camera_z = read_vector3d(m_object + 0x230)
+		local target_x, target_y, target_z = read_vector3d(m_target_object + 0x5C) -- target Location2
+		local distance = math.sqrt((target_x - player_x)^2 + (target_y - player_y)^2 + (target_z - player_z)^2) -- 3D distance
+		local local_x = target_x - player_x
+		local local_y = target_y - player_y
+		local local_z = (target_z + z_positive_offset) - player_z
+		local point_x = 1 / distance * local_x
+		local point_y = 1 / distance * local_y
+		local point_z = 1 / distance * local_z
+		local x_diff = math.abs(camera_x - point_x)
+		local y_diff = math.abs(camera_y - point_y)
+		local z_diff = math.abs(camera_z - point_z)
+		local average = (x_diff + y_diff + z_diff) / 3
+		local scaler = 0
+		if distance > 10 then scaler = math.floor(distance) / 1000 end
+		local auto_aim = sensitivity - scaler
+		if auto_aim < baseline_sensitivity then auto_aim = baseline_sensitivity end
+		if average < auto_aim then
+			return true
+		end
+	end
+	return false
+end
+
 luablam.biped = biped
 luablam.object = object
+luablam.uiWidgetDefinition = uiWidgetDefinition
+luablam.weaponHudInterface = weaponHudInterface
+luablam.unicodeStringList = unicodeStringList
+luablam.scenario = scenario
+luablam.playerIsLookingAt = playerIsLookingAt
 return luablam
+
