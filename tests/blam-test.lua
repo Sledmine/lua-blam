c9de89f2b6d5e29f59f09f5300c817e44a903335
Sledmine <40512223+Sledmine@users.noreply.github.com>
2024-03-01 03:34:44 -0600

    test cinematic globals, update unit testing

--- tests/blam-test.lua
+++ tests/blam-test.lua
@@ -1,41 +1,19 @@
+---@diagnostic disable: need-check-nil
 ------------------------------------------------------------------------------
 -- Testing for the lua blam library
 -- Sledmine, JerryBrick
--- This script must run perfectly in stock Bloodgulch!!
+-- This script must run with no issues in stock bloodgulch
+-- (bloodgulch.map) MD5: a044682640d90810a5c45afbbbb011ce 
 ------------------------------------------------------------------------------
-clua_version = 2.042
+clua_version = 2.056
 
 local lu = require "luaunit"
 local blam = require "blam"
 local glue = require "glue"
 local inspect = require "inspect"
 
--- Mocked function to redirect print calls to test print
-local function tprint(message, ...)
-    if (message) then
-        message = tostring(message)
-        if (message:find("Starting")) then
-            console_out(message)
-            return
-        end
-        console_out(message)
-    end
-end
-
---- Get index value from an id value type
----@param id number
----@return number index
-local function getIndexById(id)
-    local hex = glue.string.tohex(id)
-    local bytes = {}
-    for i = 5, #hex, 2 do
-        glue.append(bytes, hex:sub(i, i + 1))
-    end
-    return tonumber(table.concat(bytes, ""), 16)
-end
-
 function OnCommand(command)
-    if (command == "ltest") then
+    if command == "lua_blam_test" then
         local runner = lu.LuaUnit.new()
         runner:setOutputType("junit", "luablam_tests_results")
         runner:runSuite()
@@ -63,12 +41,7 @@ function testTagObjects:setUp()
     self.assaultRifleWphiTagPath = "weapons\\assault rifle\\assault rifle"
     self.assaultRifleFpAnimsTagPath = "weapons\\assault rifle\\fp\\fp"
     self.assaultRifleFpAnimsValues = {1, 6, 255, 4, 5, 2, 2, 9, 9, 255, 8, 7, 255, 3, 255, 255, 12}
-    self.defaultProfilesTagList = {
-        3909224332,
-        3909289869,
-        3909355406,
-        3909420943,
-    }
+    self.defaultProfilesTagList = {3909224332, 3909289869, 3909355406, 3909420943}
     self.multiplayerFlag = 3897165526
     self.multiplayerUnit = 3792109715
     self.firstPersonHands = 3905095503
@@ -88,6 +61,7 @@ function testTagObjects:testTag()
     lu.assertNotIsNil(cyborgMpTag, "Cyborg tag must not be nil")
     local bipedTagAddress = get_tag(blam.tagClasses.biped, self.bipedTagPath)
     lu.assertNotIsNil(cyborgMpTag.id, "Cyborg tag id must not be nil")
+    ---@diagnostic disable-next-line: undefined-field
     lu.assertEquals(cyborgMpTag.address, bipedTagAddress, "Cyborg tag address must match")
     lu.assertEquals(cyborgMpTag.path, self.bipedTagPath, "Cyborg tag path must match")
     lu.assertEquals(cyborgMpTag.class, "bipd", "Cyborg tag class type must be bipd")
@@ -173,17 +147,20 @@ function testObjects:testGetObject()
     if (player) then
         local playerBiped = blam.biped(get_object(player.objectId))
         local bipedObjectFromObjectId = blam.getObject(player.objectId)
-        lu.assertEquals(playerBiped.address, bipedObjectFromObjectId.address, "Address from biped object must match address from object id")
-        
-        local objectIndex = getIndexById(player.objectId)
+        lu.assertEquals(playerBiped.address, bipedObjectFromObjectId.address,
+                        "Address from biped object must match address from object id")
+
+        local objectIndex = blam.getIndexById(player.objectId)
         local bipedObjectFromObjectIndex = blam.getObject(objectIndex)
-        lu.assertEquals(playerBiped.address, bipedObjectFromObjectIndex.address, "Address from biped object must match address from object index")
+        lu.assertEquals(playerBiped.address, bipedObjectFromObjectIndex.address,
+                        "Address from biped object must match address from object index")
     end
 end
 
 function testObjects:testBipedObject()
     local bipedObject = blam.biped(get_dynamic_player()) --[[@as biped]]
     lu.assertNotIsNil(bipedObject)
+    ---@diagnostic disable-next-line: undefined-field
     lu.assertNotIsNil(bipedObject.structure)
     lu.assertEquals(bipedObject.invisible, false, "Biped should not have camouflage active")
     lu.assertEquals(bipedObject.shield, 1, "Biped shield should not depleted")
@@ -191,8 +168,10 @@ function testObjects:testBipedObject()
     lu.assertEquals(bipedObject.isNotDamageable, false, "Biped should be damageable")
     lu.assertEquals(bipedObject.bumpedObjectId, 0, "Biped should not be bumping other objects")
     if (blam.isNull(bipedObject.vehicleObjectId)) then
-        lu.assertIsTrue(blam.isNull(bipedObject.vehicleObjectId), "Biped should not be inside a vehicle")
-        lu.assertIsTrue(blam.isNull(bipedObject.vehicleSeatIndex), "Biped should not be seated on a vehicle")
+        lu.assertIsTrue(blam.isNull(bipedObject.vehicleObjectId),
+                        "Biped should not be inside a vehicle")
+        lu.assertIsTrue(blam.isNull(bipedObject.vehicleSeatIndex),
+                        "Biped should not be seated on a vehicle")
     else
         lu.assertIsTrue(not blam.isNull(bipedObject.vehicleObjectId), "Biped is inside a vehicle")
         lu.assertEquals(bipedObject.vehicleSeatIndex, 0, "Biped is seated on seat index 0")
@@ -240,7 +219,8 @@ function testObjects:testPlayerDataObject()
     lu.assertEquals(player.name, playerName, "Player names should be equal")
     lu.assertEquals(player.team, 0, "Player team should be red")
     lu.assertNotIsNil(player.objectId, "Player objectId mut not be nil")
-    lu.assertEquals(get_object(player.objectId), get_dynamic_player(), "Player object id address should match address from player object")
+    lu.assertEquals(get_object(player.objectId), get_dynamic_player(),
+                    "Player object id address should match address from player object")
     lu.assertNotIsNil(player.color, "Player color must not be nil")
     lu.assertEquals(player.index, 0, "Player index should be 0")
     lu.assertEquals(player.speed, 1, "Player speed should be 1")
@@ -257,46 +237,45 @@ end
 ------------------------------------------------------------------------------
 testUtilities = {}
 
-function testUtilities:testGetIndexById()
-    local player = blam.player(get_player())
-    if (player) then
-        local fmod = math.fmod
-        -- Benchamark different index substraction methods
-
-        -- Index from hex values
-        local time = os.clock()
-        local index
-        for i = 1, 2048 do
-            index = getIndexById(player.objectId)
-        end
-        local timeFromHex = os.clock() - time
-        --console_out(string.format("Index Hex based took: %f\n", timeFromHex))
-        --console_out(index)
-        
-        -- Index from calculation
-        local time = os.clock()
-        local indexFromMultiplication
-        for i = 1, 2048 do
-            -- This should be almost 8 times faster than substracting the index from hex
-            indexFromMultiplication = fmod(player.objectId, 0x10000)
-        end
-        local timeFromFormula = os.clock() - time
-        --console_out(string.format("Index Formula based took: %f\n", timeFromFormula))
-        --console_out(indexFromMultiplication)
-
-        lu.assertEquals(index, indexFromMultiplication, "Player index must match in either method")
-        lu.assertIsTrue(timeFromFormula < timeFromHex, "Index from Formula must take less time than Index from Hex")
-    end
+function testUtilities:testRotationFunctions()
+    -- local yaw, roll, matrix = eulerToRotation(90, 180)
+    -- lu.assertAlmostEquals(yaw, {x = 0.707, y = 0.707, z = 0})
 end
 
-function testUtilities:testRotationFunctions()
-    --local yaw, roll, matrix = eulerToRotation(90, 180)
-    --lu.assertAlmostEquals(yaw, {x = 0.707, y = 0.707, z = 0})
+testGameState = {}
+
+function testGameState:testCinematicGlobals()
+    execute_script("cinematic_stop")
+    local cinematic = blam.cinematicGlobals()
+    lu.assertNotIsNil(cinematic, "Cinematic globals must not be nil")
+    lu.assertIsFalse(cinematic.isInProgress, "Cinematic should not be in progress")
+    lu.assertIsFalse(cinematic.isShowingLetterbox, "Cinematic should not be showing letterbox")
+    execute_script("cinematic_start")
+    lu.assertIsTrue(cinematic.isInProgress, "Cinematic should be in progress")
+    lu.assertIsTrue(cinematic.isShowingLetterbox, "Cinematic should be showing letterbox")
+    execute_script("cinematic_stop")
+    lu.assertIsFalse(cinematic.isInProgress, "Cinematic should not be in progress")
+    lu.assertIsFalse(cinematic.isShowingLetterbox, "Cinematic should not be showing letterbox")
 end
 
 -- Mocked arguments and executions for standalone execution and in game execution
-if (not arg) then
+if not arg then
     arg = {"-v"}
     -- bprint = print
-    print = blam.consoleOutput
+    print = function(message)
+        if message:find("Failure", 1, true) then
+            blam.consoleOutput(message, table.unpack(blam.consoleColors.error))
+            return
+        elseif message:find("Starting", 1, true) then
+            blam.consoleOutput(message, table.unpack(blam.consoleColors.unknown))
+            return
+        elseif message:find("0 failure", 1, true) then
+            blam.consoleOutput(message, table.unpack(blam.consoleColors.success))
+            return
+        elseif message:find("[1-9]+ failure") then
+            blam.consoleOutput(message, table.unpack(blam.consoleColors.warning))
+            return
+        end
+        blam.consoleOutput(message)
+    end
 end
